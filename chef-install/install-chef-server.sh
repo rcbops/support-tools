862a4616cc9a32011b5af1274f565c5ef150ed7e
galstrom21 <jshepher@rackspace.com>
2013-10-17 14:45:24 -0500

    fixing typo

--- chef-install/install-chef-server.sh
+++ chef-install/install-chef-server.sh
@@ -119,8 +119,8 @@ EOF
 
     if [[ ! -e ${HOMEDIR}/.chef/knife.rb ]]; then
         /opt/chef-server/embedded/bin/knife user create ${CHEF_UNIX_USER} --disable-editing \
-        --user admin --key ${HOMEDIR}/.chef/admin.pem --user admin \
-        --admin -f ${HOMEDIR}/.chef/${CHEF_UNIX_USER}.pem -p ${CHEF_UNIX_USER_PASSWORD}
+        --user admin --key ${HOMEDIR}/.chef/admin.pem --admin \
+        -f ${HOMEDIR}/.chef/${CHEF_UNIX_USER}.pem -p ${CHEF_UNIX_USER_PASSWORD}
        cat <<EOF | /opt/chef-server/embedded/bin/knife configure -i --key ${HOMEDIR}/.chef/${CHEF_UNIX_USER}.pem --user ${CHEF_UNIX_USER}
 ${HOMEDIR}/.chef/knife.rb
 ${CHEF_URL}
